a3cf1ec270bdf9604abe786b98cb40a797ecde09
Hai T Le <hai.t.le16@gmail.com>
2019-01-19 20:45:22 -0800

    Got the socket.io to work and it is displaying perfectly now.

--- src/troll.js
+++ src/troll.js
@@ -1,28 +1,42 @@
-import React from 'react';
-import io from 'socket.io-client';
+import React from "react";
+import io from "socket.io-client";
 
-// const url = 'http://localhost:3000';
-const url = 'https://js-401-socket-io-server.herokuapp.com';
+const url = "http://localhost:3000";
+// const url = "https://js-401-socket-io-server.herokuapp.com";
 const socket = io.connect(url);
 
 class TrollJohn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      typedInput: '',
-      words: '',
+      typedInput: "",
+      words: "",
+      wordsHistory: []
     };
 
-    socket.on('incoming', payload => this.updateWords(payload));
+    socket.on("incoming", payload => this.updateWords(payload));
+    socket.on("history", payload => this.loadHistory(payload));
   }
 
+  componentDidMount() {
+    socket.emit("history");
+  }
+
+  loadHistory = history => {
+    this.setState({ wordsHistory: history });
+  };
+
   updateWords = words => {
-    this.setState({ words });
+    socket.emit("history");
+    this.setState({
+      words: words
+    });
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    socket.emit('troll', this.state.typedInput);
+    socket.emit("text", this.state.typedInput);
+    // history.push(this.state.typedInput);
   };
 
   handleNewWords = event => {
@@ -33,6 +47,14 @@ class TrollJohn extends React.Component {
     return (
       <>
         <h2>{this.state.words}</h2>
+        <ul>
+          {this.state.wordsHistory.map((record, idx) => (
+            <li key={idx}>
+              {record}
+              <button>Delete</button>
+            </li>
+          ))}
+        </ul>
         <form onSubmit={this.handleSubmit}>
           <input
             name="typedInput"
